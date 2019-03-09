157587de1e560a5546a49984655cc5201fa1f96f
unknown <jankkoziel@gmail.com>
2019-03-09 19:35:28 +0100

    Added settings file

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -1,7 +1,18 @@
-export let huffman = {
-    name: 'Huffman coding',
-    desc: 'Simple coding algorithm',
-    algorithm: (input) => { return input?input.toUpperCase():input }
+import { Settings } from '../settings'
+
+export let caesar = {
+    name: 'Caesar cipher',
+    desc: 'Simples coding algorithm',
+    config: {
+        shift: Settings.alphabet.length / 2,
+        defaultAlphabet: Settings.alphabet
+    },
+    algorithm: (input, { shift, defaultAlphabet }) => { 
+        return input.split("").map(letter => {
+            let temp = defaultAlphabet.indexOf(letter);
+            return ~temp ? defaultAlphabet[(temp + shift) % defaultAlphabet.length] : letter;
+        }).join("")
+    }
 }
 
 export let vigner = {
@@ -10,11 +21,11 @@ export let vigner = {
     algorithm: (input) => { return input }
 }
 
-export let caesar = {
-    name: 'Caesar cipher',
+export let Huffman = {
+    name: 'Huffman cipher',
     desc: 'Simples coding alorithm',
     algorithm: (input) => { return input }
-} 
+}
 
 export let atbash = {
     name: 'Atbash Cipher',
--- src/components/CipherTile.js
+++ src/components/CipherTile.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import '../styles/cipher-tile.css'
 
 class cipherTile extends Component {
-
     render() {
-        let { name, desc } = this.props.cipher
+        let { name, desc, algorithm, config } = this.props.cipher
+        let { userInput } = this.props
         return (
-
             <div className='cipher-tile'>
                 <h4 className='cipher-header'>{name}</h4>
                 <p className='cipher-desc'>{desc}</p>
                 <div className='cipher-container-input'>
-                    <p className='cipher-output'>{this.props.cipher.algorithm(this.props.userInput)}</p>
+                    <p className='cipher-output'>{userInput ? algorithm(userInput, config) : null}</p>
                 </div>
 
             </div>

ac2cadf1159524b99030ed54f79f9d8757b81892
unknown <jankkoziel@gmail.com>
2019-03-11 20:06:58 +0100

    Added Rail-fence cipher

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -17,13 +17,13 @@ export let caesar = {
 }
 
 export let atbash = {
-    name: 'Atbash Cipher',
+    name: 'Atbash cipher',
     desc: 'Originally made for Hebrew alphabet encryption, but works with any alphabet',
     config: {
         defaultAlphabet: Settings.alphabet
     },
     algorithm(input) {
-        let { defaultAlphabet, shift } = this.config
+        let { defaultAlphabet } = this.config
         return input.split("").map(letter => {
             let temp = defaultAlphabet.indexOf(letter);
             return ~temp ? defaultAlphabet[defaultAlphabet.length - 1 - temp] : letter;
@@ -31,15 +31,24 @@ export let atbash = {
     }
 }
 
-export let vigenere = {
-    name: 'Vigenére coding',
+export let railFence = {
+    name: 'Rail-fence coding',
     desc: '16th century cipher',
-    algorithm: (input) => { return input }
+    config: {
+        shift: 3
+    },
+    algorithm(input) {
+        let { shift } = this.config
+        let temp = new Array((shift)).fill('');
+        let dir = 1;
+        let counter = 0;
+        for (let letter of input) {
+            temp[counter] += letter;
+            dir = counter === 0 ? 1 : counter === shift-1 ? dir = -1 : dir;
+            counter += dir;
+        }
+        return temp.join("");
+    }
 }
 
-export let Huffman = {
-    name: 'Huffman cipher',
-    desc: 'Simples coding alorithm',
-    algorithm: (input) => { return input }
-}
 
--- src/components/CipherTile.js
+++ src/components/CipherTile.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import '../styles/cipher-tile.css'
 
 class cipherTile extends Component {
+    state = { buttonClicked: false }
+    onButtonClick = () => {
+        this.setState({
+            buttonClicked:!this.state.buttonClicked
+        })
+    }
     render() {
         let { name, desc } = this.props.cipher
         let { userInput } = this.props
+        //let buttonClass = this.state.buttonClicked ? 'cipher-button-more-clicked' : 'cipher-button-more'
+
         return (
             <div className='cipher-tile'>
                 <div className='cipher-header'>
                     <div className='cipher-more'>
-                        <button className='cipher-button-more'>
+                        <button onClick={this.onButtonClick} className='cipher-button-more'>
                             ?
                         </button>
                     </div>
@@ -17,7 +25,7 @@ class cipherTile extends Component {
                 </div>
                 <p className='cipher-desc'>{desc}</p>
                 <div className='cipher-container-output'>
-                    <p className='cipher-output'>{userInput ? this.props.cipher.algorithm(userInput) : null}</p>
+                    <input className='cipher-output' value={userInput ? this.props.cipher.algorithm(userInput) : ''} disabled/>
                 </div>
 
             </div>
